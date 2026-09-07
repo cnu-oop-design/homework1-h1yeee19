b94808b598a7abcf5f31411e41d996c7b6bd43b6
H1yeee19 <happylacu@gmail.com>
2026-09-07 10:39:01 +0900

    Fix typo in output statement for C++ readiness

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char *argv[]) {
     // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
     //
     std::cout << "Hello, OOP!" << std::endl;
-    td::cout << "C++ is ready." << std::endl;
+    std::cout << "C++ is ready." << std::endl;
 
     return 0;
 }
